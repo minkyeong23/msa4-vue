ddc7946639cb77b6e2d5e9315a61f3cf5196424d
minkyeong23 <choi777765@gmail.com>
2026-04-16 17:06:16 +0900

    260416 vue init ~ dataBinding

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// js 코드 작성 부분
+import DataBinding from './components/DataBinding.vue';
+
+</script>
+
+<template>
+<!-- HTML 작성 부분 -->
+  <DataBinding />
+</template>
+
+<style>
+/* CSS 작성 부분 */
+  
+</style>
+
+
